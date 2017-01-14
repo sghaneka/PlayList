78f84f2223b926644a791e7d4d0b86b28fea1577
Sam Ghanekar <sghaneka@yahoo.com>
2017-01-14 11:51:39 -0500

    version 1

---
+++ app/src/main/java/com/sg/playlist/PlayListFragment.java
@@ -0,0 +1,29 @@
+package com.sg.playlist;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PlayListFragment extends Fragment {
+
+
+    public PlayListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.playlist_frame, container, false);
+    }
+
+}
