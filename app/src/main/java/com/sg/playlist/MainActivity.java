06d7ff45b07f9cb68ed8005a2594b0f77c27bea6
sam g <sghaneka@yahoo.com>
2017-01-16 08:49:20 -0500

    notify button click to activity

--- app/src/main/java/com/sg/playlist/MainActivity.java
+++ app/src/main/java/com/sg/playlist/MainActivity.java
@@ -2,12 +2,19 @@ package com.sg.playlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements PlayListFragment.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        Toast.makeText(this, "selected position: " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/sg/playlist/PlayListFragment.java
+++ app/src/main/java/com/sg/playlist/PlayListFragment.java
@@ -1,17 +1,21 @@
 package com.sg.playlist;
 
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sg.MusicLayer.IMusicSource;
 import com.sg.MusicLayer.MusicSource;
@@ -27,9 +31,16 @@ public class PlayListFragment extends Fragment {
     private IMusicSource mMusicSource = new MusicSource();
     private int mSelectedButton = 0;
     private List<Button> buttons = new ArrayList<Button>();
+    private final String SELECTED_PLAYLIST = "selectedPlayList";
+    private final String LogTag = "playListLogTag";
+    private OnItemSelectedListener mListener;
 
     public PlayListFragment() {
         // Required empty public constructor
+    }
+
+    public interface OnItemSelectedListener {
+        public void onItemSelected(int position);
     }
 
 
@@ -37,7 +48,18 @@ public class PlayListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.playlist_frame, container, false);
+        View view = inflater.inflate(R.layout.playlist_frame, container, false);
+        if (savedInstanceState != null){
+            mSelectedButton = savedInstanceState.getInt(SELECTED_PLAYLIST);
+            setButtonSelected();
+        }
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        outState.putInt(SELECTED_PLAYLIST, mSelectedButton);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -53,18 +75,35 @@ public class PlayListFragment extends Fragment {
             myButton.setText(mMusicSource.getPlayLists()[i]);
             linearLayout.addView(myButton);
             buttons.add(myButton);
+            myButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int x = (int)v.getTag();
+                    mSelectedButton = x;
+                    mListener.onItemSelected(x);
+                    setButtonSelected();
+                }
+            });
         }
 
-        linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                int x = (int)v.getTag();
-                mSelectedButton = x;
-                setButtonSelected();
-            }
-        });
-
         setButtonSelected();
+        Log.d(LogTag, "on view created complete.");
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        AppCompatActivity a = null;
+
+        if (context instanceof AppCompatActivity){
+            a=(AppCompatActivity) context;
+        }
+
+        try{
+            mListener = (OnItemSelectedListener) a;
+        } catch (ClassCastException e){
+            throw new ClassCastException(a.toString() + " must implement OnItemSelectedListener." );
+        }
     }
 
     private void setButtonSelected(){
@@ -72,11 +111,12 @@ public class PlayListFragment extends Fragment {
             if ((int)b.getTag() == mSelectedButton){
                 b.setBackgroundColor(Color.CYAN);
             }else{
-                Drawable d = b.getBackground();
-                b.setBackground(d);
+                b.setBackgroundResource(android.R.drawable.btn_default);
             }
         }
     }
+
+
 
 
 }
